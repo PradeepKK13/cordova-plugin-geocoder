5eb7d99e47777410e47ffa8a5c617473f7d9148c
Maksim Chemerisuk <chemerisuk@gmail.com>
2019-02-12 11:38:47 +0300

    update ios implementation

--- src/ios/GeocoderPlugin.m
+++ src/ios/GeocoderPlugin.m
@@ -12,8 +12,12 @@
 - (void)geocode:(CDVInvokedUrlCommand *)command {
     NSString* locationName = [command.arguments objectAtIndex:0];
     NSDictionary* options = [command.arguments objectAtIndex:1];
+    NSLocale* locale = nil;
+    if (options[@"lang"]) {
+        locale = [[NSLocale alloc] initWithLocaleIdentifier:options[@"lang"]];
+    }
 
-    [_geocoder geocodeAddressString:locationName completionHandler:^(NSArray *placemarks, NSError *error) {
+    CLGeocodeCompletionHandler completionHandler = ^(NSArray *placemarks, NSError *error) {
         CDVPluginResult *pluginResult;
         if (error != nil) {
             pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.localizedDescription];
@@ -25,10 +29,48 @@
             pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsArray:results];
         }
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-    }];
+    };
+
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 110000
+    if (@available(iOS 11.0, *)) {
+        [_geocoder geocodeAddressString:locationName inRegion:nil preferredLocale:locale completionHandler:completionHandler];
+    }
+#else
+    [_geocoder geocodeAddressString:locationName completionHandler:completionHandler];
+#endif
 }
 
 - (void)reverseGeocode:(CDVInvokedUrlCommand *)command {
+    double latitude = [[command.arguments objectAtIndex:0] doubleValue];
+    double longitude = [[command.arguments objectAtIndex:1] doubleValue];
+    NSDictionary* options = [command.arguments objectAtIndex:2];
+    CLLocation* location = [[CLLocation alloc] initWithLatitude:latitude longitude:longitude];
+    NSLocale* locale = nil;
+    if (options[@"lang"]) {
+        locale = [[NSLocale alloc] initWithLocaleIdentifier:options[@"lang"]];
+    }
+
+    CLGeocodeCompletionHandler completionHandler = ^(NSArray *placemarks, NSError *error) {
+        CDVPluginResult *pluginResult;
+        if (error != nil) {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.localizedDescription];
+        } else {
+            NSMutableArray* results = [[NSMutableArray alloc] init];
+            for (CLPlacemark* placemark in placemarks) {
+                [results addObject:[self placemarkToDictionary:placemark]];
+            }
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsArray:results];
+        }
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    };
+
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 110000
+    if (@available(iOS 11.0, *)) {
+        [_geocoder reverseGeocodeLocation:location preferredLocale:locale completionHandler:completionHandler];
+    }
+#else
+    [_geocoder reverseGeocodeLocation:location completionHandler:completionHandler];
+#endif
 }
 
 - (NSDictionary*)placemarkToDictionary:(CLPlacemark *)placemark {
